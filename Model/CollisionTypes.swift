f6e9fe20bf729590333bb556a8a1d0bacecd10d9
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-31 12:26:53 +0300

    init commit

---
+++ Model/CollisionTypes.swift
@@ -0,0 +1,15 @@
+//
+//  CollisionTypes.swift
+//  Exploding Monkeys
+//
+//  Created by Evgeniy Ryshkov on 31/12/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import Foundation
+
+enum CollisionTypes: UInt32 {
+    case banana = 1
+    case building = 2
+    case player = 4
+}
---
+++ View/BuildingNode.swift
@@ -0,0 +1,19 @@
+//
+//  BuildingNode.swift
+//  Exploding Monkeys
+//
+//  Created by Evgeniy Ryshkov on 31/12/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class BuildingNode: SKSpriteNode {
+    
+    var currentImage: UIImage!
+    
+    func setup() {
+        
+    }
+}
