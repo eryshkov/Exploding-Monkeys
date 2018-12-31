42bdceddd17ea14ba307319dd7ba34123b53578a
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-31 19:18:10 +0300

    assets added

--- Controller/GameScene.swift
+++ Controller/GameScene.swift
@@ -14,6 +14,12 @@ class GameScene: SKScene {
     var buildings = [BuildingNode]()
     weak var viewController: GameViewController!
     
+    var player1: SKSpriteNode!
+    var player2: SKSpriteNode!
+    var banana: SKSpriteNode!
+    
+    var currentPlayer = 1
+    
     //MARK: -
     func createBuildings() {
         var currentX: CGFloat = -15
@@ -35,6 +41,32 @@ class GameScene: SKScene {
     
     func launch(angle: Int, velocity: Int) {
         
+    }
+    
+    func createPlayers() {
+        player1 = SKSpriteNode(imageNamed: "player")
+        player1.name = "player1"
+        player1.physicsBody = SKPhysicsBody(circleOfRadius: player1.size.width / 2)
+        player1.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player1.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        player1.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        player1.physicsBody?.isDynamic = false
+        
+        let player1Building = buildings[1]
+        player1.position = CGPoint(x: player1Building.position.x, y: player1Building.position.y + ((player1Building.size.height + player1.size.height) / 2))
+        addChild(player1)
+        
+        player2 = SKSpriteNode(imageNamed: "player")
+        player2.name = "player2"
+        player2.physicsBody = SKPhysicsBody(circleOfRadius: player2.size.width / 2)
+        player2.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+        player2.physicsBody?.collisionBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.contactTestBitMask = CollisionTypes.banana.rawValue
+        player2.physicsBody?.isDynamic = false
+        
+        let player2Building = buildings[buildings.count - 2]
+        player2.position = CGPoint(x: player2Building.position.x, y: player2Building.position.y + ((player2Building.size.height + player2.size.height) / 2))
+        addChild(player2)
     }
     
     //MARK: -
