6d98390425ef5d35aec24c60213f7825e1348bd3
Evgeniy Ryshkov <eryshkov@gmail.com>
2018-12-31 13:24:18 +0300

    drawBuilding(size: CGSize) -> UIImage added

--- View/BuildingNode.swift
+++ View/BuildingNode.swift
@@ -14,7 +14,42 @@ class BuildingNode: SKSpriteNode {
     var currentImage: UIImage!
     
     func drawBuilding(size: CGSize) -> UIImage {
-        return UIImage()
+        
+        let renderer = UIGraphicsImageRenderer(size: size)
+        let img = renderer.image { (ctx) in
+            let rectangle = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+            var color: UIColor
+            
+            switch Int.random(in: 0...2) {
+            case 0:
+                color = UIColor(hue: 0.502, saturation: 0.98, brightness: 0.67, alpha: 1)
+            case 1:
+                color = UIColor(hue: 0.999, saturation: 0.99, brightness: 0.67, alpha: 1)
+            default:
+                color = UIColor(hue: 0, saturation: 0, brightness: 0.67, alpha: 1)
+            }
+            
+            ctx.cgContext.setFillColor(color.cgColor)
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fill)
+            
+            let lightOnColor = UIColor(hue: 0.190, saturation: 0.67, brightness: 0.99, alpha: 1)
+            let lightOffColor = UIColor(hue: 0, saturation: 0, brightness: 0.34, alpha: 1)
+            
+            for row in stride(from: 10, to: Int(size.height - 10), by: 40) {
+                for col in stride(from: 10, to: Int(size.width - 10), by: 40) {
+                    if Bool.random() {
+                        ctx.cgContext.setFillColor(lightOnColor.cgColor)
+                    }else{
+                        ctx.cgContext.setFillColor(lightOffColor.cgColor)
+                    }
+                    ctx.cgContext.fill(CGRect(x: col, y: row, width: 15, height: 20))
+                }
+            }
+            //4
+        }
+        
+        return img
     }
     
     func configurePhysics() {
